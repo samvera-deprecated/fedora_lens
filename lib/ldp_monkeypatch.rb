13b3cf5ddd374e81cebc68568edab2b090b7d24e
Brian Maddy <brian@brianmaddy.com>
2014-04-01 12:02:36 -0500

    added comments about the monkeypatches

--- lib/ldp_monkeypatch.rb
+++ lib/ldp_monkeypatch.rb
@@ -3,12 +3,16 @@ require 'active_support/core_ext/object'
 module Ldp
   module Response
     def self.resource? raw_resp
+      # workaround for a bug in fedora-commons, see https://github.com/cbeer/ldp/issues/1
       links(raw_resp).fetch("type", []).to_set.intersection([Ldp.resource.to_s, "http://www.w3.org/ns/ldp/Resource"]).present?
     end
   end
 
   class Resource
     def create
+      # create seems to not be working in ldp, but it's not entirely clear how it should work
+      # for example, :new? calls :get, which will raise an error when an object doesn't exist,
+      # this means it is impossible to create anything.
       # raise "" if new?
       resp = client.post '/rest', graph.dump(:ttl) do |req|
         # req.headers['Slug'] = subject
