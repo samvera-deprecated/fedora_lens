621c65acf32f93b7ce1b458a9091755af1e81a1d
Justin Coyne <justin@curationexperts.com>
2014-07-22 09:59:43 -0500

    Clear etag after fetching attributes post create

--- lib/fedora_lens.rb
+++ lib/fedora_lens.rb
@@ -136,9 +136,15 @@ module FedoraLens
     def create_record
       push_attributes_to_orm
       @orm = orm.create
+
       # This is slow, but it enables us to get attributes like http://fedora.info/definitions/v4/repository#created
       # TODO perhaps attributes could be lazily fetched
       @attributes = get_attributes_from_orm(@orm)
+
+      # This strips the ETag from the current ORM to avoid ETag exceptions on the next update,
+      # since the etag will change if a child node is added.
+      @orm = Ldp::Orm.new @orm.resource.class.new @orm.resource.client, @orm.resource.subject
+
       true
     end
 
